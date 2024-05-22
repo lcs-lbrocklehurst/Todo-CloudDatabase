c144aee5ce1ea332fbcef11682e1b9aa80b97e4b
Lewis Brocklehurst <lbrocklehurst@lcs.on.ca>
2024-05-22 10:32:22 -0400

    Adjusted view model to pull to-do items from the database hosted at Supabase.

--- TodoList/Helpers/Supabase.swift
+++ TodoList/Helpers/Supabase.swift
@@ -6,3 +6,9 @@
 //
 
 import Foundation
+import Supabase
+
+let supabase = SupabaseClient(
+  supabaseURL: URL(string: "https://jzzsvnlewkgsqeslbwfe.supabase.co")!,
+  supabaseKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp6enN2bmxld2tnc3Flc2xid2ZlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTYzMTc5ODEsImV4cCI6MjAzMTg5Mzk4MX0.hRt2-mdCW-c7LJ0-dhDWXfTSA-na2wGM0XDplWsZDeA"
+)
--- TodoList/ViewModel/TodoListViewModel.swift
+++ TodoList/ViewModel/TodoListViewModel.swift
@@ -6,3 +6,59 @@
 //
 
 import Foundation
+
+@Observable
+class TodoListViewModel {
+    
+    // MARK: Stored properties
+    // The list of to-do items
+    var todos: [TodoItem]
+    
+    // MARK: Initializer(s)
+    init(todos: [TodoItem] = []) {
+        self.todos = todos
+        Task {
+            try await getTodos()
+        }
+    }
+    
+    // MARK: Functions
+    func getTodos() async throws {
+    
+    do {
+        let results: [TodoItem] = try await supabase
+            .from("todos")
+            .select()
+            .execute()
+            .value
+        
+        self.todos = results
+        
+    } catch {
+        debugPrint(error)
+    }
+    
+}
+    func createToDo(withTitle title: String) {
+        
+        // Create the new to-do item instance
+        let todo = TodoItem(
+            title: title,
+            done: false
+        )
+        
+        // Append to the array
+        todos.append(todo)
+        
+    }
+    
+    func delete(_ todo: TodoItem) {
+        
+        // Remove the provided to-do item from the array
+        todos.removeAll { currentItem in
+            currentItem.id == todo.id
+        }
+        
+    }
+    
+}
