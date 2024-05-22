e7f6cb31c8c5a076a4adcb416c1c046d3f2f5d3f
Lewis Brocklehurst <lbrocklehurst@lcs.on.ca>
2024-05-22 10:09:42 -0400

    Added a helper file to identify our app to the Supabase servers.

---
+++ TodoList/Helpers/Supabase.swift
@@ -0,0 +1,8 @@
+//
+//  Supabase.swift
+//  TodoList
+//
+//  Created by Lewis Brocklehurst on 2024-05-22.
+//
+
+import Foundation
---
+++ TodoList/ViewModel/TodoListViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  TodoListViewModel.swift
+//  TodoList
+//
+//  Created by Lewis Brocklehurst on 2024-05-21.
+//
+
+import Foundation
